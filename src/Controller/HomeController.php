1791c2476d76cb08c4f5187a81cfb57897dfab74
Nico FRANCOIS <nfrancois45@gmail.com>
2018-10-29 14:09:31 +0100

    fin quete3 symfony

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wilder15
+ * Date: 29/10/18
+ * Time: 11:44
+ */
+
+namespace App\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/")
+     */
+
+    public function index()
+    {
+
+        return $this->render('home/home.html.twig');
+    }
+}
+
+
+
---
+++ src/Controller/LuckyController.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wilder15
+ * Date: 29/10/18
+ * Time: 11:44
+ */
+
+namespace App\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+// src/Controller/LuckyController.php
+
+// ...
+class LuckyController extends AbstractController
+{
+    /**
+     * @Route("/lucky/number")
+     */
+    public function number()
+    {
+        $number = random_int(0, 100);
+
+        return $this->render('lucky/number.html.twig', [
+            'number' => $number,
+        ]);
+    }
+}
