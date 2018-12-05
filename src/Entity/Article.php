93ba918e7bfc4160be977aed4720d396fd579db0
Nico FRANCOIS <nfrancois45@gmail.com>
2018-12-05 16:52:50 +0100

    80%queteok

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -112,4 +112,11 @@ class Article
 
         return $this;
     }
+    public function __toString()
+    {
+        // to show the name of the Category in the select
+        return $this->title;
+        // to show the id of the Category in the select
+        // return $this->id;
+    }
 }
